59af0a7392fc175a5088effc400b2bdffc49d326
Nicky2811 <carcamojade@gmail.com>
2026-02-08 11:17:16 -0600

    paso 7

---
+++ practica3/src/hooks/useFetchMovieDetails.js
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { API_KEY } from "./useFetchMovies";
+
+/**
+@param {string} selectedId
+@returns {Object}
+*/
+export function useFetchMovieDetails(selectedId){
+    const [movie, setMovie] = useState({}); 
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(()=>{
+        if (!selectedId) {
+            setMovie({});
+            setError("");
+            return;
+        }
+        /** @param {string} selectedId */
+
+        async function fetchMovieDetails(selectedId) {
+            try{
+                setIsLoading(true);
+                setError(null);
+
+                const response = await
+            fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`);
+
+            if (!response.ok)
+                throw new Error ("Error al cargar los detalles de la pelicula");
+
+            const data = await response.json();
+
+            setMovie(data);
+            } catch (err) {
+                setError(err.message);
+                setMovie({});
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchMovieDetails(selectedId);
+    }, [selectedId]);
+
+    return {movie, isLoading, error}; 
+}
